37b2cb31490bce75f3cb8fdfbeb064ac68d6c503
sbondada <sbondada@buffalo.edu>
2013-08-15 15:09:14 -0400

    added the complete tripartie graph

--- TripartiteGraph/TripartiteGraph.cpp
+++ TripartiteGraph/TripartiteGraph.cpp
@@ -1,4 +1,4 @@
-#include <boost/config.hpp> // put this first to suppress some VC++ warnings
+#include <boost/config.hpp> // kept it to suppress some VC++ warnings
 
 #include <iostream>
 #include <iterator>
@@ -7,6 +7,7 @@
 
 #include <boost/utility.hpp>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 using namespace boost;
@@ -16,11 +17,18 @@ namespace tptg
 
 enum type{subject,verb,object};
 
-struct TripartiteGrapgparam
+struct TripartiteGraphVertexParam
 {
-	std::vector<std::string> subjectlist;
-	std::vector<std::string> verblist;
-	std::vector<std::string> objectlist;
+	std::vector<std::string> subjectvlist;
+	std::vector<std::string> verbvlist;
+	std::vector<std::string> objectvlist;
+};
+
+struct TripartiteGraphEdgeParam
+{
+
+	std::vector<std::pair<std::string,int> > subjectverbelist;
+	std::vector<std::pair<std::string,int> > verbobjectelist;
 };
 
 struct edgeprop
@@ -43,36 +51,77 @@ class TripartiteGraph
 private:
 
 UndirectedGraph udgraph;
-TripartiteGrapgparam param;
+TripartiteGraphVertexParam vparam;
+TripartiteGraphEdgeParam eparam;
 Collection subjlist;
 Collection verlist;
 Collection objlist;
 
 public:
 
-TripartiteGraph(UndirectedGraph& tptgraph,TripartiteGrapgparam& parameters):udgraph(tptgraph),param(parameters){}
+TripartiteGraph(UndirectedGraph& tptgraph,TripartiteGraphVertexParam& vparameters,TripartiteGraphEdgeParam& eparameters):udgraph(tptgraph),vparam(vparameters),eparam(eparameters){}
 
 void constructvertex()
 {
 	 typedef typename UndirectedGraph::vertex_property_type Name;
-	 for(int i=0;i<sizeof(param.subjectlist);i++)
+	 for(int i=0;i<sizeof(vparam.subjectvlist);i++)
 	 {
-		 subjlist.nodelist.push_back(add_vertex(Name(param.subjectlist[i]),udgraph));
+		 subjlist.nodelist.push_back(add_vertex(Name(vparam.subjectvlist[i]),udgraph));
 	 }
-	 for(int i=0;i<sizeof(param.verblist);i++)
+	 for(int i=0;i<sizeof(vparam.verbvlist);i++)
 	 {
-		 verlist.nodelist.push_back(add_vertex(Name(param.verblist[i]),udgraph));
+		 verlist.nodelist.push_back(add_vertex(Name(vparam.verbvlist[i]),udgraph));
 	 }
-	 for(int i=0;i<sizeof(param.objectlist);i++)
+	 for(int i=0;i<sizeof(vparam.objectvlist);i++)
 	 {
-		 objlist.nodelist.push_back(add_vertex(Name(param.objectlist[i]),udgraph));
+		 objlist.nodelist.push_back(add_vertex(Name(vparam.objectvlist[i]),udgraph));
 	 } 
 	 return;
 }
 
 void constructedge()
 {
-	
+	std::vector<std::string> splitstr;
+	typename graph_traits<UndirectedGraph>::vertex_descriptor src,tgt;
+	int matchsrc,matchtgt;
+	for(int i=0;i<eparam.subjectverbelist.size();i++)
+	{
+	boost::split(splitstr,eparam.subjectverbelist[i].first,boost::is_any_of("-"));
+	if(splitstr.size()==2)
+	{
+		matchsrc=getIndex(vparam.subjectvlist,splitstr[0]);
+		matchsrc=getIndex(vparam.verbvlist,splitstr[1]);
+		if(matchsrc!=-1 && matchtgt!=-1)
+		{
+		src=subjlist.nodelist[matchsrc];
+		tgt=verlist.nodelist[matchtgt];
+		typename graph_traits<UndirectedGraph>::edge_descriptor e1;
+		bool found;
+		boost::tie(e1,found)=add_edge(src,tgt,udgraph); 
+		udgraph[e1].edgename=eparam.verbobjectelist[i].first;
+		udgraph[e1].weight=eparam.verbobjectelist[i].second;
+		}	
+	}
+	}
+	for(int i=0;i<eparam.verbobjectelist.size();i++)
+	{
+	boost::split(splitstr,eparam.verbobjectelist[i].first,boost::is_any_of("-"));
+	if(splitstr.size()==2)
+	{
+		matchsrc=getIndex(vparam.verbvlist,splitstr[0]);
+		matchsrc=getIndex(vparam.objectvlist,splitstr[1]);
+		if(matchsrc!=-1 && matchtgt!=-1)
+		{
+		src=verlist.nodelist[matchsrc];
+		tgt=objlist.nodelist[matchtgt];
+		typename graph_traits<UndirectedGraph>::edge_descriptor e1;
+		bool found;
+		boost::tie(e1,found)=add_edge(src,tgt,udgraph); 
+		udgraph[e1].edgename=eparam.verbobjectelist[i].first;
+		udgraph[e1].weight=eparam.verbobjectelist[i].second;
+		}	
+	}
+	}
 }
 
 int getIndex(std::vector<std::string>& collist,std::string matchstr)
@@ -91,6 +140,7 @@ int getIndex(std::vector<std::string>& collist,std::string matchstr)
     }
     return -1;
 }
+
 edgeprop getEdgeDescription(std::string source,std::string target,type sourcetype)
 {
 	typename property_map < UndirectedGraph,std::string edgeprop::*>::type
@@ -101,8 +151,8 @@ edgeprop getEdgeDescription(std::string source,std::string target,type sourcetyp
 	int matchsrc,matchtgt;
 	if(sourcetype==subject)
 	{
-	matchsrc=getIndex(param.subjectlist,source);
-	matchtgt=getIndex(param.verblist,target);
+	matchsrc=getIndex(vparam.subjectvlist,source);
+	matchtgt=getIndex(vparam.verbvlist,target);
 		if(matchsrc!=-1 && matchtgt!=-1)
 		{
 		src=subjlist.nodelist[matchsrc];
@@ -111,14 +161,15 @@ edgeprop getEdgeDescription(std::string source,std::string target,type sourcetyp
 	}	
 	else if(sourcetype==verb)
 	{
-	matchsrc=getIndex(param.verblist,source);
-	matchtgt=getIndex(param.objectlist,target);
+	matchsrc=getIndex(vparam.verbvlist,source);
+	matchtgt=getIndex(vparam.objectvlist,target);
 		if(matchsrc!=-1 && matchtgt!=-1)
 		{
 		src=verlist.nodelist[matchsrc];
 		tgt=objlist.nodelist[matchtgt];
 		}
 	}	
+	//if(src// add the test case
 	typename graph_traits<UndirectedGraph>::edge_descriptor e1,e2;
 	bool found;
 	boost::tie(e1,found)=edge(src,tgt,udgraph);
@@ -136,17 +187,17 @@ std::string getVertexDescription(std::string nodename,type wordtype)
 	int matchnode;
 	if(wordtype==subject)
 	{
-		matchnode=getIndex(param.subjectlist,nodename);
+		matchnode=getIndex(vparam.subjectvlist,nodename);
 		vertexnode=subjlist.nodelist[matchnode];
 	}
 	else if(wordtype==verb)
 	{
-		matchnode=getIndex(param.verblist,nodename);
+		matchnode=getIndex(vparam.verbvlist,nodename);
 		vertexnode=verlist.nodelist[matchnode];
 	}
 	else 
 	{
-		matchnode=getIndex(param.objectlist,nodename);
+		matchnode=getIndex(vparam.objectvlist,nodename);
 		vertexnode=objlist.nodelist[matchnode];
 	}
 	return get(vname,vertexnode);
